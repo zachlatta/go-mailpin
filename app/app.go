a91e029a18448c5d04ef605d9d8e2829834f4081
Zach Latta <zach@zachlatta.com>
2014-05-07 13:36:33 -0700

    Put incoming email logic into its own file.

--- app/app.go
+++ app/app.go
@@ -1,25 +1,24 @@
 package mailpin
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"io/ioutil"
 	"net/http"
-	"net/mail"
 
 	"github.com/gorilla/mux"
+	"github.com/zachlatta/go-mailpin/controller/email"
 	"github.com/zachlatta/go-mailpin/model"
 	"github.com/zachlatta/go-mailpin/view"
 
 	"appengine"
-	"appengine/datastore"
 )
 
 func init() {
 	r := mux.NewRouter()
+
 	r.Handle("/", model.AppHandler(root)).Methods("GET")
 	r.Handle("/{id}", model.AppHandler(viewEmail)).Methods("GET")
-	r.Handle("/_ah/mail/", model.AppHandler(incomingMail)).Methods("POST")
+	r.Handle("/_ah/mail/", model.AppHandler(email.IncomingHandler)).
+		Methods("POST")
+
 	http.Handle("/", r)
 }
 
@@ -38,36 +37,5 @@ func viewEmail(w http.ResponseWriter, r *http.Request) *model.AppError {
 	}
 
 	w.Write(page.Body)
-	return nil
-}
-
-func incomingMail(w http.ResponseWriter, r *http.Request) *model.AppError {
-	c := appengine.NewContext(r)
-	defer r.Body.Close()
-
-	msg, err := mail.ReadMessage(r.Body)
-	if err != nil {
-		return &model.AppError{err, "Error parsing email", http.StatusBadRequest}
-	}
-
-	bytes, err := ioutil.ReadAll(msg.Body)
-	if err != nil {
-		return &model.AppError{err, "Internal server error",
-			http.StatusInternalServerError}
-	}
-
-	hasher := md5.New()
-	hasher.Write(bytes)
-	id := hex.EncodeToString(hasher.Sum(nil))
-
-	page := model.Page{
-		Subject: msg.Header["Subject"][0],
-		Body:    bytes,
-	}
-
-	key := model.NewPageKey(c, id)
-	if _, err := datastore.Put(c, key, &page); err != nil {
-		return &model.AppError{err, "Error storing page", http.StatusInternalServerError}
-	}
 	return nil
 }
---
+++ controller/email/incoming.go
@@ -0,0 +1,45 @@
+package email
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"net/http"
+	"net/mail"
+
+	"github.com/zachlatta/go-mailpin/model"
+
+	"appengine"
+	"appengine/datastore"
+)
+
+func IncomingHandler(w http.ResponseWriter, r *http.Request) *model.AppError {
+	c := appengine.NewContext(r)
+	defer r.Body.Close()
+
+	msg, err := mail.ReadMessage(r.Body)
+	if err != nil {
+		return &model.AppError{err, "Error parsing email", http.StatusBadRequest}
+	}
+
+	bytes, err := ioutil.ReadAll(msg.Body)
+	if err != nil {
+		return &model.AppError{err, "Internal server error",
+			http.StatusInternalServerError}
+	}
+
+	hasher := md5.New()
+	hasher.Write(bytes)
+	id := hex.EncodeToString(hasher.Sum(nil))
+
+	page := model.Page{
+		Subject: msg.Header["Subject"][0],
+		Body:    bytes,
+	}
+
+	key := model.NewPageKey(c, id)
+	if _, err := datastore.Put(c, key, &page); err != nil {
+		return &model.AppError{err, "Error storing page", http.StatusInternalServerError}
+	}
+	return nil
+}
