eb154e2c0086517e80327f7eb266298e4311d5ed
Zach Latta <zach@zachlatta.com>
2014-05-09 15:13:57 -0700

    Send response email.

--- controller/email/incoming.go
+++ controller/email/incoming.go
@@ -3,6 +3,7 @@ package email
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/mail"
@@ -11,6 +12,7 @@ import (
 
 	"appengine"
 	"appengine/datastore"
+	appMail "appengine/mail"
 )
 
 func IncomingHandler(w http.ResponseWriter, r *http.Request) *model.AppError {
@@ -20,6 +22,12 @@ func IncomingHandler(w http.ResponseWriter, r *http.Request) *model.AppError {
 	msg, err := mail.ReadMessage(r.Body)
 	if err != nil {
 		return &model.AppError{err, "Error parsing email", http.StatusBadRequest}
+	}
+
+	addresses, err := msg.Header.AddressList("From")
+	if err != nil {
+		return &model.AppError{err, "Error parsing 'From' field in email",
+			http.StatusBadRequest}
 	}
 
 	bytes, err := ioutil.ReadAll(msg.Body)
@@ -39,7 +47,29 @@ func IncomingHandler(w http.ResponseWriter, r *http.Request) *model.AppError {
 
 	key := model.NewPageKey(c, id)
 	if _, err := datastore.Put(c, key, &page); err != nil {
-		return &model.AppError{err, "Error storing page", http.StatusInternalServerError}
+		return &model.AppError{err, "Error storing page",
+			http.StatusInternalServerError}
 	}
+
+	notifyMsg := &appMail.Message{
+		Sender:  "go-mailpin <noreply@go-mailpin.appspot.com>",
+		To:      addressesToStrings(addresses),
+		Subject: "Mailpin",
+		Body:    fmt.Sprintf("https://go-mailpin.appspot.com/%s", id),
+	}
+
+	if err := appMail.Send(c, notifyMsg); err != nil {
+		return &model.AppError{err, "Error sending mail",
+			http.StatusInternalServerError}
+	}
+
 	return nil
+}
+
+func addressesToStrings(addresses []*mail.Address) []string {
+	strings := make([]string, len(addresses))
+	for i, address := range addresses {
+		strings[i] = address.String()
+	}
+	return strings
 }
